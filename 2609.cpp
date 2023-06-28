b72e0368a9ea380c76244ceb10beb8698ab2a356
SeokHyunY <slam9381@naver.com>
2023-06-29 05:51:16 +0900

    틀린 문제(유클리드 알고리즘)

---
+++ 2609.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    int c = a % b;
+
+    while(c != 0) {
+        a = b;
+        b = c;
+        c = a % b;
+    }
+    return b;
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
+int main() {
+    int num1, num2;
+    cin >> num1 >> num2;
+
+    cout << gcd(num1, num2) << "\n" << lcm(num1, num2) << "\n";
+}
